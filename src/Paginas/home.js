b08708c1c2161f74ba0b58cb53a35ec4bc187ea1
EstebanBel29 <estebanbeltran29@gmail.com>
2023-09-10 22:30:26 -0300

    Primeras ediciones del proyecto

---
+++ src/Paginas/home.js
@@ -0,0 +1,26 @@
+//hoja con estilos
+import './estiloshome.css';
+//paquetes-modulos
+
+//componentes
+
+
+
+function Home() {
+    return(
+            <div className='div-container'>
+                <h1>Proyect Rick & Morty</h1>
+                <h2>Helcome to Rick & Morty Proyect!</h2>
+                <p>This proyect was made for practising React and to made a functional websit</p>
+                <p>In this website you can know information of the characters of this animated series.</p>
+                <p>Also you can filter for diferent types of properties to find the character that you are looking for or send us a massage for any concern o sugestion</p>
+                <h3>Lets go!</h3>
+                <div className='botones'>
+                    <button>characters</button>
+                    <button>Contact</button>
+                </div>
+            </div>
+    )
+}
+
+export default Home;
